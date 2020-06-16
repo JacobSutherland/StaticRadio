a1c3eeac29f5a12261cef13714c28bec2e9484e7
Jacob <Jacobsutherland224@gmail.com>
2020-06-16 01:49:22 -0500

    fixed drop down in log in

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const dropDown = document.querySelector('.drop-down');
 const exitIcon = document.querySelector('.close-menu')
-const header = document.querySelector('#home header');
+const header = document.querySelector('header:nth-of-type(1)');
 const nav = document.querySelector('.header-nav');
 const headerLinks = document.querySelectorAll('.header-nav a');
-const homeSection = document.querySelector('#home');
-const logo = document.querySelector('.logo');
-
-//Counter enables us to toggle styles on and off depending on whether it is even or odd.
-let counter = 1;
 
 dropDown.addEventListener('click', () =>{
-    //toggles navigation to mobile display on click
+    //opens navigation to mobile display on click
     dropDown.style.display = 'none';
     exitIcon.style.display = 'block';
     exitIcon.classList.toggle('close');
@@ -20,10 +15,10 @@ dropDown.addEventListener('click', () =>{
     for(let i = 0; i < headerLinks.length; i++){
     headerLinks[i].style.display = 'initial';
     }
-    console.log(counter);
+    console.log('lick');
 });
 
-
+//closes drop down menu on close icon click
 exitIcon.addEventListener('click', () => {
     dropDown.style.display = 'block';
     exitIcon.style.display = 'none';
@@ -35,3 +30,18 @@ exitIcon.addEventListener('click', () => {
     headerLinks[i].style.display = 'none';
     }
 })
+
+// Closes drop down menu if an option is choosen 
+for(let i = 0; i < headerLinks.length; i++){
+    headerLinks[i].addEventListener('click', () => {
+        dropDown.style.display = 'block';
+        exitIcon.style.display = 'none';
+        exitIcon.classList.toggle('close');
+        nav.classList.toggle('mobile-nav');
+        header.style.padding = '40px';
+    
+        for(let i = 0; i < headerLinks.length; i++){
+        headerLinks[i].style.display = 'none';
+        }    
+    })
+}
